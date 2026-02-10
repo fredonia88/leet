20bbe7d10d37f0fe83579ea949b8f0fef8040795
Frederick Jones <fjjones88@gmail.com>
2026-02-09 21:11:33 -0600

    found faster solution

--- medium/7_reverse_integer.py
+++ medium/7_reverse_integer.py
@@ -1,3 +1,5 @@
+import time
+
 class Solution:
     def reverse(self, x: int) -> int:
         sign = -1 if x < 0 else 1
@@ -7,10 +9,29 @@ class Solution:
             return 0
         else:
             return y
+        
+class SolutionFaster:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        y = 0
+        while x:
+            y = y * 10 + x % 10
+            x //= 10
+        y *= sign
+        return y if -2**31 <= y <= 2**31 else 0
 
 if __name__ == '__main__':
     text_x = 123
 
     sol = Solution()
+    t0 = time.perf_counter()
     result = sol.reverse(text_x)
-    print(result)
+    t1 = time.perf_counter()
+    print('Solution result:', result, 'time:', t1 - t0)
+
+    sol = SolutionFaster()
+    t0 = time.perf_counter()
+    result = sol.reverse(text_x)
+    t1 = time.perf_counter()
+    print('Solution result:', result, 'time:', t1 - t0)
